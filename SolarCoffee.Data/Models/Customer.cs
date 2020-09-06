31770632506190a9ad85cf7e3f2a22c52b31c5ce
OSBORNE Luke <losborne@amaris.com>
2020-09-06 19:49:51 +0300

    feat: add migrations feat: add makefile feat: add customer classes

---
+++ SolarCoffee.Data/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SolarCoffee.Data.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedTime { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public CustomerAddress PrimaryAddress { get; set; }
+    }
+}
--- SolarCoffee.Web/Startup.cs
+++ SolarCoffee.Web/Startup.cs
@@ -1,31 +1,36 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using SolarCoffee.Data;
+using System.IO;
 
 namespace SolarCoffee.Web
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private IConfiguration _configuration { get; set; }
+        public Startup(IWebHostEnvironment environment)
         {
-            Configuration = configuration;
+            _configuration = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", false, true)
+                 .AddJsonFile("appsettings.json", false, true).Build();
         }
 
-        public IConfiguration Configuration { get; }
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<SolarDbContext>(options =>
+            {
+                options.EnableDetailedErrors();
+                options.UseSqlServer(_configuration.GetConnectionString("solar.dev"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
