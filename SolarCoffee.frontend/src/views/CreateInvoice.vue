4967351ab4e719cbf8734e8d744a80ea4bb160e2
OSBORNE Luke <Luke.osborne@live.com>
2020-09-29 23:30:45 +0300

    feat: update invoice form

--- SolarCoffee.frontend/src/views/CreateInvoice.vue
+++ SolarCoffee.frontend/src/views/CreateInvoice.vue
@@ -23,21 +23,155 @@
     </div>
 
     <div class="invoice-step" v-if="invoiceStep === 2">
-      <p>setp 2</p>
+      <h2>Step 2: Create Order</h2>
+
+      <div v-if="inventory.length" class="invoice-step-detail">
+        <label for="product">Product:</label>
+        <select v-model="newItem.product" class="invoiceLineItem" id="product">
+          <option disabled value="">Please select a Product</option>
+          <option v-for="i in inventory" :value="i.product" :key="i.product.id">
+            {{ i.product.name }}
+          </option>
+        </select>
+        <label for="quantity">Quantity:</label>
+        <input v-model="newItem.quantity" id="quantity" type="number" min="0" />
+      </div>
+
+      <div class="invoice-item-actions">
+        <solar-button
+          :disabled="!newItem.product || !newItem.quantity"
+          @button:click="addLineItem"
+        >
+          Add Line Item
+        </solar-button>
+
+        <solar-button
+          :disabled="!lineItems.length"
+          @button:click="finalizeOrder"
+        >
+          Finalize Order
+        </solar-button>
+      </div>
+
+      <div class="invoice-order-list" v-if="lineItems.length">
+        <div class="runningTotal">
+          <h3>Running Total:</h3>
+          {{ runningTotal | price }}
+        </div>
+        <hr />
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Product</th>
+              <th>Description</th>
+              <th>Qty.</th>
+              <th>Price</th>
+              <th>Subtotal</th>
+            </tr>
+          </thead>
+          <tr
+            v-for="lineItem in lineItems"
+            :key="`index_${lineItem.product.id}`"
+          >
+            <td>{{ lineItem.product.name }}</td>
+            <td>{{ lineItem.product.description }}</td>
+            <td>{{ lineItem.quantity }}</td>
+            <td>{{ lineItem.product.price }}</td>
+            <td>{{ (lineItem.product.price * lineItem.quantity) | price }}</td>
+          </tr>
+        </table>
+      </div>
     </div>
 
-    <div class="invoice-step" v-if="invoiceStep === 3">
-      <p>step 3</p>
+   <div class="invoice-step" v-if="invoiceStep === 3">
+      <h2>Step 3: Review and Submit</h2>
+      <solar-button @button:click="submitInvoice">Submit Invoice</solar-button>
+      <hr />
+
+      <div class="invoice-step-detail" id="invoice" ref="invoice">
+        <div class="invoice-logo">
+          <!-- <img
+            id="imgLogo"
+            alt="Solar Coffee logo"
+            src="../assets/images/solar_coffee_logo.png"
+          /> -->
+          <h3>1337 Solar Lane</h3>
+          <h3>San Somewhere, CA 90000</h3>
+          <h3>USA</h3>
+
+          <div class="invoice-order-list" v-if="lineItems.length">
+            <div class="invoice-header">
+              <h3>Invoice: {{ new Date() | humanizeDate }}</h3>
+              <h3>
+                Customer:
+                {{
+                  this.selectedCustomer.firstName +
+                    " " +
+                    this.selectedCustomer.lastName
+                }}
+              </h3>
+              <h3>
+                Address: {{ this.selectedCustomer.primaryAddress.addressLine1 }}
+              </h3>
+              <h3 v-if="this.selectedCustomer.primaryAddress.addressLine2">
+                {{ this.selectedCustomer.primaryAddress.addressLine2 }}
+              </h3>
+              <h3>
+                {{ this.selectedCustomer.primaryAddress.city }},
+                {{ this.selectedCustomer.primaryAddress.state }},
+                {{ this.selectedCustomer.primaryAddress.postalCode }}
+              </h3>
+              <h3>
+                {{ this.selectedCustomer.primaryAddress.country }}
+              </h3>
+            </div>
+            <table class="table">
+              <thead>
+                <tr>
+                  <th>Product</th>
+                  <th>Description</th>
+                  <th>Qty.</th>
+                  <th>Price</th>
+                  <th>Subtotal</th>
+                </tr>
+              </thead>
+              <tr
+                v-for="lineItem in lineItems"
+                :key="`index_${lineItem.product.id}`"
+              >
+                <td>{{ lineItem.product.name }}</td>
+                <td>{{ lineItem.product.description }}</td>
+                <td>{{ lineItem.quantity }}</td>
+                <td>{{ lineItem.product.price }}</td>
+                <td>
+                  {{ (lineItem.product.price * lineItem.quantity) | price }}
+                </td>
+              </tr>
+              <tr>
+                <th colspan="4"></th>
+                <th>Grand Total</th>
+              </tr>
+              <tfoot>
+                <tr>
+                  <td colspan="4" class="due">Balance due upon receipt:</td>
+                  <td class="price-final">{{ runningTotal | price }}</td>
+                </tr>
+              </tfoot>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
     <hr />
-    <div class="invoice-step-actions">
-      <solar-button @button:click="prev" :disabled="!canGoPrev">
-        Previous
-      </solar-button>
-      <solar-button @button:click="next" :disabled="!canGoNext">
-        Next
-      </solar-button>
-      <solar-button @button:click="statOver">Start Over</solar-button>
+
+    <div class="invoice-steps-actions">
+      <solar-button @button:click="prev" :disabled="!canGoPrev"
+        >Previous</solar-button
+      >
+      <solar-button @button:click="next" :disabled="!canGoNext"
+        >Next</solar-button
+      >
+      <solar-button @button:click="startOver">Start Over</solar-button>
     </div>
   </div>
 </template>
@@ -57,7 +191,7 @@ const inventoryService = new InventoryService();
 const invoiceService = new InvoiceService();
 
 @Component({
-  name: "Create Invoice",
+  name: "CreateInvoice",
   components: {
     SolarButton
   }
@@ -114,9 +248,20 @@ export default class CreateInvoice extends Vue {
     return false;
   }
 
-  canGoPrev() {}
+  get canGoPrev() {
+    return this.invoiceStep !== 1;
+  }
 
-  startOver() {}
+  startOver(): void {
+    this.invoice = {
+      customerId: 0,
+      lineItems: [],
+      createdDate: new Date(),
+      updatedDate: new Date()
+    };
+    this.invoiceStep = 1;
+    this.selectedCustomerId = 0;
+  }
 }
 </script>
 
